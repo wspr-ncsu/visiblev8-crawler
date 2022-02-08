24670279d491689c2b4d33ea4dbfb4b44c0ffb13
John Fagan <jgfagan@ncsu.edu>
2022-02-07 22:03:55 -0500

    Create and test Dockerfile
    Added a Dockerfile to build the web server as a docker image.
    Tested running the docker image with

--- vv8web/server.py
+++ vv8web/server.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 
-from routers import api_v1, webpage
+from vv8web.routers import api_v1, webpage
 
 app = FastAPI()
-
-print('webpage:', type(webpage.router), webpage.router)
-print('api:', type(api_v1.router), api_v1.router)
 
 app.include_router(webpage.router)
 app.include_router(api_v1.router)

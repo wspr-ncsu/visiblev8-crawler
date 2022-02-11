3290a143537828c6898abdea5d171a487882016b
sbriri <bma6@ncsu.edu>
2022-02-10 20:04:50 -0500

    animation implemented

--- vv8web/resources/webpages/scripts/ani.js
+++ vv8web/resources/webpages/scripts/ani.js
@@ -1,15 +1,6 @@
-let progressValue = 0;
-const progressBar = document.querySelector(".current-progress");
-
-progressBar.style.width = `${progressValue}%`;
-
-const timer = setInterval(() => {
-    if (progressValue < 100) {
-        progressValue += 10;
-        progressBar.style.width = `${progressValue}%`;
-
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {    
+        $(".loading").hide();
+        $('body').css('overflow','scroll');
     }
-    if (progressValue === 100) {
-        clearInterval(timer);
-    }
-}, 1000);
+  }

c8e71b6c6210ef872649678cbfc54aaf657105b9
Ryan Rice <rnrice2@ncsu.edu>
2022-02-14 12:27:16 -0500

    Began implementing Javascript on the index page

--- web_server/vv8web/resources/webpages/scripts/main.js
+++ web_server/vv8web/resources/webpages/scripts/main.js
@@ -1,2 +1,14 @@
-// Get the submit button
-var submitButton = document.getElementById("submitButton");
+// On form submit, alert the user of the form data.
+function submitForm() {
+    var form = document.getElementById("search-container");
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    // xhr.open("POST", "/submit", true);
+    // xhr.onload = function() {
+    //     if (xhr.status === 200) {
+    //         alert(xhr.responseText);
+    //     }
+    // };
+    // xhr.send(formData);
+    alert(formData);
+}

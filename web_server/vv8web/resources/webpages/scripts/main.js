944f1a62efe5cb731e4a67e2230358c85cacf7d1
Ryan Rice <rnrice2@ncsu.edu>
2022-02-14 16:53:58 -0500

    Updated the form and added JS to react to text input

--- web_server/vv8web/resources/webpages/scripts/main.js
+++ web_server/vv8web/resources/webpages/scripts/main.js
@@ -3,12 +3,23 @@ function submitForm() {
     var form = document.getElementById("search-container");
     var formData = new FormData(form);
     var xhr = new XMLHttpRequest();
-    // xhr.open("POST", "/submit", true);
+
+    // Send the form data to the server.
+    // The server is FastAPI
+    // The URL is "/api/v1/url"
+    xhr.open("POST", "/api/v1/url", true);
+    xhr.send(formData);
+
+    // xhr.open("POST", "/api/v1/url", true);
     // xhr.onload = function() {
     //     if (xhr.status === 200) {
     //         alert(xhr.responseText);
     //     }
     // };
     // xhr.send(formData);
-    alert(formData);
+    alert(formData.get("url"));
 }
+
+// Add event listener to form
+const form = document.getElementById("search-container");
+form.addEventListener("submit", submitForm);

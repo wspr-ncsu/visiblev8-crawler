7e6ecdae5c7dc4c4684560767c519a8266f59e73
rnrice2 <rnrice2@ncsu.edu>
2022-02-19 22:44:59 -0500

    Added HTML files to the server

--- web_server/vv8web/server.py
+++ web_server/vv8web/server.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 from vv8web.routers import api_v1, webpage
 
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory="vv8web/static", html=True), name="static")
 
 app.include_router(webpage.router)
 app.include_router(api_v1.router)

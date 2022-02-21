9a0992e4f68f98a8e44db4b44482fc18ada2e176
sbriri <bma6@ncsu.edu>
2022-02-21 01:00:43 -0500

    Add dropdown bar
    Far from finish

--- web_server/vv8web/resources/webpages/scripts/ani.js
+++ web_server/vv8web/resources/webpages/scripts/ani.js
@@ -1,9 +1,12 @@
-// document.onreadystatechange = function () {
-//     if (document.readyState == "complete") {
-//         $(".loading").hide();
-//         $('body').css('overflow','scroll');
-//     }
-//   }
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        $(".loading").hide();
+        $('body').css('overflow','scroll');
+    }
+  }
+
+
+
 
 function logBreaker() {
 
@@ -27,5 +30,36 @@ function logBreaker() {
       
     })
 }
+
+
+
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}
+
+	var accordion = new Accordion($('#accordion'), false);
+});
+
+
 
 logBreaker()

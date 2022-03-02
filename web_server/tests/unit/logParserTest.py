08f3026d9a9766808d3bcd1b6d0975fc85f9d5b9
Quentin Davidson <qsd10qsd@gmail.com>
2022-03-02 14:34:14 -0500

    Changed pathNames from .JSON to .json

--- web_server/tests/unit/logParserTest.py
+++ web_server/tests/unit/logParserTest.py
@@ -6,11 +6,11 @@ import filecmp
 
 class LogParserTests(unittest.TestCase):
     def test_parser_accuracy(self):
-        testPath = "C:\\Users\\qsd10\\PycharmProjects\\2022SpringTeam17-Kapravelos-LAS-1\\web_server\\logs\\testResult-https-__coinsbit.io.txt.JSON"
-        absPath = "C:\\Users\\qsd10\\PycharmProjects\\2022SpringTeam17-Kapravelos-LAS-1\\web_server\\logs\\https-__coinsbit.io.txt"
+        testPath = "C:\\Users\\qsd10\\PycharmProjects\\2022SpringTeam17-Kapravelos-LAS-1\\web_server\\logs\\testResult-https-__coinsbit.io.txt.json"
+        absPath = "C:\\Users\\qsd10\\PycharmProjects\\2022SpringTeam17-Kapravelos-LAS-1\\web_server\\logs\\https-__coinsbit.io.txt.json"
         web_server.vv8web.logParser.main(absPath)
-        self.assertTrue(os.path.exists(absPath + ".JSON"))  # add assertion here
-        self.assertTrue(filecmp.cmp(absPath + ".JSON", testPath, False))
+        self.assertTrue(os.path.exists(absPath))  # add assertion here
+        self.assertTrue(filecmp.cmp(absPath, testPath, False))
 
 
 if __name__ == '__main__':

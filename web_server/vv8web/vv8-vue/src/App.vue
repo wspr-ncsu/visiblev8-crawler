5d107c66661a38b59837327fc2a3700398d33b1e
sbriri <bma6@ncsu.edu>
2022-03-21 13:34:20 -0400

    add result.vue

--- web_server/vv8web/vv8-vue/src/App.vue
+++ web_server/vv8web/vv8-vue/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Layout from '@/components/Layout.vue'
+import Index from '@/components/Index.vue'
 </script>
 
 <template>
---
+++ web_server/vv8web/vv8-vue/src/components/Result.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-row>
+    <el-col :span="24"><head></head><div class="grid-content bg-purple-dark" /></el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12">a<div class="grid-content bg-purple" /></el-col>
+    <el-col :span="12">b<div class="grid-content bg-purple-light" /></el-col>
+  </el-row>
+</template>
+
+<style>
+
+
+</style>
+

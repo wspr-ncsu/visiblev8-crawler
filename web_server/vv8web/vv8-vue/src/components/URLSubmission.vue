33df5f4b412696e237e7edb33def63a0e903ab85
sbriri <bma6@ncsu.edu>
2022-03-26 12:50:12 -0400

    started working on frontedn api

--- web_server/vv8web/vv8-vue/src/components/URLSubmission.vue
+++ web_server/vv8web/vv8-vue/src/components/URLSubmission.vue
@@ -1,11 +1,34 @@
 <template>
     <form class="index-search-container" id="search-container">
         <input name="request" type="text" placeholder="Enter URL here">
-        <button type="submit"><i class="fa fa-search"></i></button>
+        <button type="submit" @click="passText"><i class="fa fa-search"></i></button>
     </form>    
 </template>
 
 <script setup>
+
+// On form submit, alert the user of the form data.
+function reqListener() {
+    console.log(this.responseText)
+}
+
+function submitForm() {
+    var urlForm = document.getElementById("search-container");
+    var endpoint = "/api/v1/url"
+    var formData = new FormData(urlForm);
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", reqListener);
+
+    // Send the form data to the server.
+    xhr.open("POST", endpoint, true);
+    xhr.send(formData);
+    alert(formData.get("request"));
+}
+
+// Add event listener to form
+// const form = document.getElementById("search-container");
+// form.addEventListener("submit", submitForm);
+
 
 
 </script>

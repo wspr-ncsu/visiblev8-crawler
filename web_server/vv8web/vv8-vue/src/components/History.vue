5671627ce081ad360f626ccd475944c36da8a109
qdavids <qdavids@ncsu.edu>
2022-03-30 14:13:20 -0400

    Header for HIstory and About + Results demo setup
    Set up History and About pages with the header, Results page loading bar works, and demo systems added to click on loading bar and swap to results layout

--- web_server/vv8web/vv8-vue/src/components/History.vue
+++ web_server/vv8web/vv8-vue/src/components/History.vue
@@ -1,9 +1,15 @@
-<script setup>
-
+<script>
+	import ElementPlus from 'element-plus'
+	import Header from "./Header.vue"
 </script>
 
 <template>
-  <h1>History</h1>
+  <div>
+		<el-row>
+			<el-col :span="24"><Header /></el-col>
+		</el-row>
+    <h1>History</h1>
+  </div>
 </template>
 
 <style>

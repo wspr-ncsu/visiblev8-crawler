bbc1ba60659e64c8e9588cbaaa69036832ff022a
Ryan Rice <rnrice2@ncsu.edu>
2022-04-05 23:35:12 -0400

    Fixed the prefix for the URL Submission

--- web_server/vv8web/vv8-vue/src/components/URLSubmission.vue
+++ web_server/vv8web/vv8-vue/src/components/URLSubmission.vue
@@ -1,38 +1,46 @@
 <template>
-    <!-- <form class="index-search-container" id="search-container">
-        <el-input v-model="input" name="request" type="text" placeholder="Enter URL here" />
-        <button type="submit" @click="passText"><i class="fa fa-search"></i></button>
-    </form>     -->
-    <div class="index-search-container" id="search-container">
-    <el-input
-      v-model="input3"
-      placeholder="Enter URL here"
-      class="input-with-select"
-      name="request"
-      type="text"
-    >
-      <template #prepend>
-        <el-select v-model="select" placeholder="Select" style="width: 110px">
-          <el-option label="http://" value="http://" />
-          <el-option label="https://" value="http://" />
-        </el-select>
-      </template>
-      <template #append>
-        <el-button :icon="Search" type="submit" @click="passText"/>
-      </template>
-    </el-input>
-  </div>
+  <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form-item>
+      <el-input v-model="formInline.url" placeholder="Enter URL (ex: google.com)" style="min-width: 500px">
+        <template #prepend>
+          <el-select v-model="formInline.prefix" placeholder="Prefix" style="width: 90px">
+            <el-option label="http://" value="http://" />
+            <el-option label="https://" value="https://" />
+          </el-select>
+        </template>
+        <template #append>
+          <el-button :icon="Search" type="primary" @click="onSubmit"/>
+        </template>
+      </el-input>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { Search } from '@element-plus/icons-vue'
-const input3 = ref('')
-const select = ref('')
+import { sendURL } from '@/apis/subURL'
+import { reactive } from 'vue'
 
-import sendURL from '@/apis/subURL'
-const passText = () =>{
-    // here we use api
+const formInline = reactive({
+  prefix: '',
+  url: '',
+})
+
+const onSubmit = () => {
+  // if the prefix is empty, we will alert the user
+  if (formInline.prefix === '') {
+    alert('Please enter a prefix')
+    return
+  }
+  // if the url is empty, we will alert the user
+  else if (formInline.url === '') {
+    alert('Please enter a url')
+    return
+  }
+  else {
+    sendURL(formInline.prefix + formInline.url)
+  }
 }
 
 </script>

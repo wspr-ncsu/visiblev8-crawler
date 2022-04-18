06aa429ec9af8d46bf24e92f8bed7bd601a7c57c
Scott Burnett <sburnet@vv8web.csc.ncsu.edu>
2022-04-18 12:31:23 -0400

    Fleshed out an api_v1 test for database side car

--- database_sidecar/tests/unit/api_v1_test.py
+++ database_sidecar/tests/unit/api_v1_test.py
@@ -1,47 +1,65 @@
 import unittest
-from web_server.vv8web.routers import api_v1 as api_v1
-from web_server.vv8web.routers import webpage as webpage
+from urllib.parse import urlparse
+from vv8db_sidecar.routers.api_v1 import api_v1
+from vv8db_sidecar.models.parsed_log_model import ParsedLogModel
+from vv8db_sidecar.models.submission_model import SubmissionModel
+from vv8db_sidecar.models.submission_response_model import SubmissionResponseModel
 
 
-# Testing api_v1 and a little bit of webpage, sending a valid webpage and two invalid ones to ensure
-# our backend url validation works correctly.
+# Testing api_v1 in the Database sidecar. We need to test two functions: postsubmission and post_parsed_log
 class BackendApiTests(unittest.TestCase):
-    def test_api_connections(self):
+    def test_postsubmission(self):
 
-        # Valid URL
-        googleWeb = "https://www.google.com"
-        # Invalid http/https scheme
-        invalidWeb = "htpps://www.google.com"
-        # Invalid URL
-        invalidWeb2 = "http://www.ggogle.com"
+        # Import our test documents and parsed test documents
+        log_files = [
+            'akamai.net.txt',
+            'https-__appleinsider.com.txt',
+            'https-__coinsbit.io.txt',
+            'https-__iqoption.txt'
+        ]
+        parsed_files = [
+            'akamai.net_parsed.json',
+            'https-__appleinsider.com_parsed.json',
+            'https-__coinsbit.io_parsed.json',
+            'https-__iqoption_parsed.json'
+        ]
 
-        # Run valid URL, not quite sure what this will return but hopefully pass/fail or bool
-        hopetrue = api_v1.postUrl(googleWeb)
-        # Test that Results were posted TODO
-        self.assertIsNotNone(webpage.get_results())
+        # Create new submission model for testing
+        testSub = SubmissionModel('akamai.net.txt')
 
-        # Check to make sure that URL was valid
-        self.assertTrue(hopetrue.valid)  # add assertion here
-
-        # Run invalid URL, not quite sure what this will return but hopefully pass/fail
-        hopefail = api_v1.postUrl(invalidWeb)
-        # Test that no Results were posted TODO
+        # Send the submission model through postsubmission
+        # Check to make sure the response is of type SubmissionResponseModel
+        # (No failure testing bc we assume any url that has made it this far is valid)
         try:
-            self.assertIsNone(webpage.get_results())
-        except FileNotFoundError:
-            self.itpass(self)
-        # Check to make sure that URL was invalid
-        self.assertFalse(hopefail.valid)
+            subReturn = api_v1.postsubmission(testSub)
+            self.assertIsInstance(subReturn, SubmissionResponseModel)
+        except BaseException:
+            self.fail("dbsidecar.api_vi_test.test_postsubmission has failed.")
 
-        # Run invalid URL, not quite sure what this will return but hopefully pass/fail
-        hopefail2 = api_v1.postUrl(invalidWeb2)
-        # Test that no Results were posted TODO
+    def test_post_parsed_log(self):
+        # Import our test documents and parsed test documents
+        log_files = [
+            'akamai.net.txt',
+            'https-__appleinsider.com.txt',
+            'https-__coinsbit.io.txt',
+            'https-__iqoption.txt'
+        ]
+        parsed_files = [
+            'akamai.net_parsed.json',
+            'https-__appleinsider.com_parsed.json',
+            'https-__coinsbit.io_parsed.json',
+            'https-__iqoption_parsed.json'
+        ]
+
+        # Create new parsed log model for testing
+        testSub = ParsedLogModel
+        testSub.url = 'akamai.net_parsed.json'
+
+        # Submit testSub, then check the database(?) to see if it was submitted?
         try:
-            self.assertIsNone(webpage.get_results())
-        except FileNotFoundError:
-            self.itpass(self)
-        # Check to make sure that URL was invalid
-        self.assertFalse(hopefail2.valid)
+            subReturn = api_v1.post_parsed_log(testSub)
+        except BaseException:
+            self.fail("dbsidecar.api_vi_test.test_post_parsed_log has failed.")
 
     def itpass(self):
         return
@@ -49,7 +67,3 @@ class BackendApiTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
-#    vv8 worker
-#    api_v1
-#    task queue unit tests

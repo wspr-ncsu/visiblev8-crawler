d8a606eda852f43acdacff876ff649b504ee7bb1
Your Name <you@example.com>
2023-01-23 21:29:49 +0000

    Use session instead of engine

--- database_sidecar/vv8db_sidecar/db_conn_manager.py
+++ database_sidecar/vv8db_sidecar/db_conn_manager.py
@@ -2,8 +2,9 @@ import vv8db_sidecar.config as cfg
 import urllib.parse
 import atexit
 import asyncio
-
+from sys import stderr
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 _esc_db_user = urllib.parse.quote(cfg.database_username)
 _esc_db_password = urllib.parse.quote(cfg.database_password)
@@ -13,7 +14,7 @@ _esc_db_name = urllib.parse.quote(cfg.database_name)
 
 _engine_url = f'postgresql+asyncpg://{_esc_db_user}:{_esc_db_password}@{_esc_db_host}:{_esc_db_port}/{_esc_db_name}'
 engine = create_async_engine(_engine_url, pool_size=20, max_overflow=0)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # TODO: investigate need to dispose of engine at module close
 # I've investgated adding a blocking call to async function to atexit, but it is difficult to get the event loop
 # from synchronous code.

c919196a1088391f4aecf55a5796728e92f9b898
Sohom <sohomdatta1+git@gmail.com>
2023-04-04 08:35:39 -0400

    Make crawler work without headless mode
    - Add xvfb dependency
    - Add parameters to load extension in crawler.js

--- celery_workers/vv8_worker/vv8_crawler/crawler.js
+++ celery_workers/vv8_worker/vv8_crawler/crawler.js
@@ -2,7 +2,7 @@ const { URL } = require('url');
 const puppeteer = require('puppeteer-extra');
 const PuppeteerHar = require('puppeteer-har');
 const PuppeteerExtraPluginStealth = require('puppeteer-extra-plugin-stealth');
-const fs = require( 'fs' )
+const fs = require( 'fs' );
 
 // Tuning parameters
 // Per Juestock et al. 30s + 15s for page load
@@ -14,10 +14,12 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 // CLI entry point
 function main() {
     const program = require('commander');
-
     const default_crawler_args = [
                     "--disable-setuid-sandbox",
                     "--no-sandbox",
+                    //' --enable-logging=stderr',
+                    '--enable-automation',
+                    //'--v=1'
                 ];
     const crawler_args = process.argv.slice(6);
 
@@ -25,34 +27,49 @@ function main() {
         .version('1.0.0');
     program
         .command("visit <URL> <uid>")
-        .option( '--headless <headless>', 'Run in headless mode', 'new')
+        .option( '--headless <headless>', 'Which headless mode to run visiblev8', 'new')
         .option( '--loiter-time <loiter_time>', 'Amount of time to loiter on a webpage', DEFAULT_LOITER_TIME)
         .option( '--nav-time <nav_time>', 'Amount of time to wait for a page to load', DEFAULT_NAV_TIME)
         .allowUnknownOption(true)
         .description("Visit the given URL and store it under the UID, creating a page record and collecting all data")
-        .action(async function(input_url, uid, headless, loiter_time, nav_time) {
+        .action(async function(input_url, uid, options) {
             let combined_crawler_args = default_crawler_args.concat(crawler_args);
+            let show_log = false;
+            const user_data_dir = `/tmp/${uid}`;
+
+            if ( combined_crawler_args.includes( '--show-chrome-log' ) ) {
+                show_log = true;
+                combined_crawler_args = combined_crawler_args.filter( function( item ) {
+                    return item !== '--show-chrome-log';
+                } );
+            }
+
+            if ( combined_crawler_args.includes( '--no-headless' ) ) {
+                options.headless = false;
+                combined_crawler_args = combined_crawler_args.filter( function( item ) {
+                    return item !== '--no-headless';
+                } );
+            }
 
             puppeteer.use(PuppeteerExtraPluginStealth());
             const browser = await puppeteer.launch({
-                headless: headless,
-                userDataDir: `/tmp/${uid}`,
+                headless: options.headless,
+                userDataDir: user_data_dir,
+                dumpio: show_log,
                 executablePath: '/opt/chromium.org/chromium/chrome',
                 args: combined_crawler_args
             });
 
-            const page = await browser.newPage();
+            const page = await browser.newPage( { viewport: null } );
             const har = new PuppeteerHar(page);
             const url = new URL(input_url);
             try {
                 await har.start({ path: `${uid}.har` });
                 await page.goto(url, {
-                    timeout: nav_time * 1000,
+                    timeout: options.navTime * 1000,
                     waitUntil: 'networkidle0'
                 });
-
-                await sleep(loiter_time * 1000);
-
+                await sleep(options.loiterTime * 1000);
                 await page.screenshot({path: `./${uid}.png`});
                 
 
@@ -63,10 +80,12 @@ function main() {
             await har.stop()
             await page.close();
             await browser.close();
+            console.log('here')
             // Throw away user data
-            await fs.promises.rmdir(`/tmp/${uid}`, { recursive: true, force: true });
+            await fs.promises.rmdir(user_data_dir, { recursive: true, force: true });
             
         });
+    //xvfb.stopSync();
     program.parse(process.argv);
 }
 

3d61f553c558cb0251c768f88c19ea6a6a4cd4fa
anahape <akaprav@ncsu.edu>
2024-02-08 10:43:38 -0500

    Extra Celery flags

--- celery_workers/vv8_worker/entrypoint.sh
+++ celery_workers/vv8_worker/entrypoint.sh
@@ -3,5 +3,5 @@ rm /tmp/.X1-lock 2> /dev/null &
 /opt/noVNC/utils/novnc_proxy --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
 # Insecure option is needed to accept connections from the docker host.
 vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
-celery -A vv8_worker.app worker -Q crawler -l INFO -c $CELERY_CONCURRENCY -Ofair --max-tasks-per-child 1 &
+celery -A vv8_worker.app worker -Q crawler -l INFO -c $CELERY_CONCURRENCY -Ofair --max-tasks-per-child 1 $EXTRA_CELERY_ARGS &
 wait
